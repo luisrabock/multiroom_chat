95612894df6fb70fe4e5e32c4eb2f5c79f515bf0
Luís <luis.rabock@hotmail.com>
2018-02-05 13:42:47 -0200

    Add files via upload

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const server = app.listen(3001, () => {
 
 const io = require('socket.io').listen(server)
 
+app.set('io', io)
+
 /*criar conexao socket io */
 
 io.on('connection',(socket) => {
@@ -18,5 +20,35 @@ io.on('connection',(socket) => {
     socket.on('disconnect', () => {
         console.log('Usuario desconectou')
     })
+
+    socket.on('msgParaServidor', (data) => {
+
+        /*dialogo*/
+        socket.emit('msgParaCliente',
+        {apelido: data.apelido, mensagem: data.mensagem}
+
+        )
+
+        socket.broadcast.emit('msgParaCliente',
+        {apelido: data.apelido, mensagem: data.mensagem}
+
+        )
+        /*participantes*/
+        if(parseInt(data.apelido_atualizado_clientes) == 0) {
+        socket.emit('participantesParaClientes',
+        {apelido: data.apelido}
+
+        )
+
+        socket.broadcast.emit('participantesParaClientes',
+        {apelido: data.apelido}
+
+        )
+    }
+
+    })
+
+
+
 
 })
